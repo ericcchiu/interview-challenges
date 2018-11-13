258f89ea05f88d1607e0397c438e51d6af9df2c1
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-13 09:28:52 -0600

    (doc & feat) Initialized repo and add first interview challenge on searching for a str in an array.

---
+++ 1.1 Searching for a string in an array.js
@@ -0,0 +1,35 @@
+// Given a string and an array of strings, return true if the string exists in the array, and false if it does not.
+// DO NOT use .includes() in your solution.
+// DO use a for loop.
+// TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+
+// For of loop 
+// const searchString = (str, arrStr) => { 
+//   for (let item of arrStr) { 
+//     if (item === str) return true; 
+//   }
+//   return false; 
+// }
+
+// Recursion 
+// const searchString = (str, arrStr) => { 
+//   if (arrStr.length === 0) return false; 
+  
+//   if (arrStr[0] === str) return true; 
+
+//   return searchString(str, arrStr.slice(1));
+// };
+
+// Reduce 
+const searchString = (str, arrStr) => { 
+   return arrStr.reduce((isFound, item ) => { 
+     return isFound || item === str; 
+   }, false); 
+}
+
+
+
+
+const arr = ['hello', 'matt', 'what', 'is', 'up'];
+console.log(searchString('matt', arr)); // Expect true 
+console.log(searchString('nope', arr)); // Expect false 
