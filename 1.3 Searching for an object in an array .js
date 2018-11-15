930119e4ace6b5ceab066e4d29ae45e1b3423159
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-15 09:14:04 -0600

    (feat) Add 1.3 Searching for an object in an array

---
+++ 1.3 Searching for an object in an array .js
@@ -0,0 +1,26 @@
+// Given a name (string) and an array of people (objects), find the person 
+// with the given name and return his/her hobby. Return false if there is no person with the given name.
+
+/*** ES6 ***/
+// const findPerson = (name, people) => { 
+//   for (let person of people) { 
+//     if (person.name === name) return person.hobby; 
+//   }
+//   return false; 
+// }; 
+
+/*** Recursion ***/
+const findPerson = (name, people) => { 
+  
+  if (people[0] === undefined) return false; 
+  
+  if (people[0].name === name) return people[0].hobby; 
+  
+  return findPerson(name, people.slice(1));
+
+}
+
+const people = [ { "name": "Alice", "hobby": "programming" }, { "name": "Bob", "hobby": "gaming" } ];
+
+console.log(findPerson('Alice', people)); 
+console.log(findPerson('Alice', people)); 
