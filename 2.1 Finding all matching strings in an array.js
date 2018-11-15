f8d50b616793ffd7e9054feab81cef4f8b5b6919
ericcchiu <eric.chn.chiu@gmail.com>
2018-11-15 09:30:04 -0600

    (refactor) Change 2.1 to work with recursion.

--- 2.1 Finding all matching strings in an array.js
+++ 2.1 Finding all matching strings in an array.js
@@ -1,3 +1,28 @@
 // Given a length (number) and an array of strings, return all strings that are greater 
 // than or equal to the given length.
+/*** ES6 ***/
+// const filterLength = (length, array) => { 
+//   const outputArr = []; 
+//   for (let str of array) { 
+//     if (str.length >= length) { 
+//       outputArr.push(str); 
+//     }
+//   }
+//   return outputArr;
+// }; 
 
+/*** Recursive ***/ 
+const filterLength = (length, array) => { 
+  const outputArr = []; 
+  if (array.length === 0) return []; 
+  if (array[0].length >= length) outputArr.push(array[0]); 
+
+  return outputArr.concat(filterLength(length, array.slice(1)));
+};
+
+
+
+const arr = [ "a", "ab", "abc", "abcd" ]; 
+const arr2 = ['x', 'hello', 'z'];
+console.log(filterLength(3, arr));
+console.log(filterLength(3, arr2));
