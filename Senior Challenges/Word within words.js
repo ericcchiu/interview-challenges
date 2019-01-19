014dcd0c3c2462cf9fac1fc10638158835a0b033
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-18 19:50:37 -0600

    (feat) Add working words within words toy problem.

---
+++ Senior Challenges/Word within words.js
@@ -0,0 +1,23 @@
+const nestedWordCount = (wordList) => {
+  let maxCount = 0;
+  let count, bestWord;
+
+  for (let wordIndex = 0; wordIndex < wordList.length; wordIndex++) {
+    let curWord = wordList[wordIndex];
+    let copyList = wordList.slice();
+    copyList.splice(wordIndex, 1);
+    count = 0;
+
+    for (let letter = 0; letter < copyList.length; letter++) {
+      if (curWord.includes(copyList[letter])) count++;
+      if (count > maxCount) {
+        maxCount = count;
+        bestWord = curWord;
+      }
+    }
+  }
+  return bestWord;
+};
+
+const arr1 = ["gray", "grays", "ray", "rays", "strays"];
+console.log(nestedWordCount(arr1)); 
